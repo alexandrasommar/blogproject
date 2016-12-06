92a16496afe51a78ac27a717970b13072499ab7a
Tobias Setterborg <setterborg.tobias@gmail.com>
2016-12-06 15:39:52 +0100

    Lagt in koden för slidren i slider.php och sedan inkluderat den i index.php.

---
+++ slider.php
@@ -0,0 +1,23 @@
+<div class="slider">
+		<div class="icon">
+					<div class="iconfont"><i class="fa fa-5x fa-car" aria-hidden="true"></i></div>
+					<div class="text">
+						<h3>Lorem ipsum</h3>
+						<p>Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet.</p>
+					</div>
+				</div>
+				<div class="icon">
+					<div class="iconfont"><i class="fa fa-5x fa-recycle" aria-hidden="true"></i></div>
+					<div class="text">
+						<h3>Lorem ipsum</h3>
+						<p>Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet.</p>
+					</div>
+				</div>
+				<div class="icon">
+					<div class="iconfont"><i class="fa fa-5x fa-usd" aria-hidden="true"></i></div>
+					<div class="text">
+						<h3>Lorem ipsum</h3>
+						<p>Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet.</p>
+					</div>
+				</div>
+	</div>
