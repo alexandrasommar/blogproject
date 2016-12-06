845142b4587070bc3a79e2d74ba260f5ee9f8c92
Tobias Setterborg <setterborg.tobias@gmail.com>
2016-12-06 12:18:19 +0100

    Lagt in include header-navigation-menu.php och header.php.

---
+++ header.php
@@ -0,0 +1,11 @@
+	
+				<div class="header__text">
+					<div class="header__text--title fadeInRight">
+						<span>Volvo <i class="fa fa-heart" aria-hidden="true"></i> Eko</span>
+					</div>
+					<div class="header__text--text">
+						<span>Det är ett välkänt faktum att läsare</span>
+					</div>
+				</div> <!-- .header__text -->
+		</header>
+	</div>
--- index.php
+++ index.php
@@ -3,83 +3,17 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Document</title>
+	<title>Volvo loves eco</title>
 	<!-- Regular CSS -->
 	<link rel="stylesheet" href="css/style.css">
 	<!-- Google Fonts -->
 	<link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
 </head>
-<body>
+
+<?php include "header-navigation-menu.php" ?>
+<?php include "header.php" ?>
 	
-	<!-- Header -->
-	<div class="header__container">
-		<header>
-			<div class="header-menu__container">
-				<div class="header-container">
 
-				<!-- Header navigation left -->
-
-				<div class="header-menu header-menu__left">
-					<nav>
-						<ul>
-							<li><a href="#">Bilen</a></li>
-							<li><a href="#">Kampanjen</a></li>
-							<li><a href="category.html">Kategorier</a></li>
-						</ul>
-					</nav>
-				</div> <!-- .header-menu & header-menu__left -->
-
-				<!-- Header navigation center -->
-
-				<div class="header-menu header-menu__logo">
-		                <a href="index.html"><img class="logo-img" src="img/Volvo_Logos_Iron_Mark.png" alt="Volvo"></a>         
-				</div> <!-- .header-menu & header-menu__logo -->
-				
-				<!-- Responsive navigation. Hidden if bigger than 800px -->
-
-				<div class="header-menu toggle-menu">
-					<input type="checkbox" id="menu-toggle">
-						<label for="menu-toggle" class="label-toggle"></label>
-					</input>
-					<div class="hidden">
-		                	<div class="toggle-menu__list">
-									<ul class="toggle-menu__flex-center">
-										<li><a href="#">Bilen</a></li>
-										<li><a href="#">Kampanjen</a></li>
-										<li><a href="category.html">Kategorier</a></li>
-										<li><a href="#">Arkiv</a></li>
-										<li><a href="#">Kontakt</a></li>
-										<li><a href="dashboard/index.html">Dashboard</a></li>
-										<li><a href="#">Logga ut</a></li>
-									</ul>
-								</div>
-		                </div>
-				</div>
-		
-				<!-- Header navigation right -->
-
-				<div class="header-menu header-menu__right">
-		        	<nav>
-		                <ul>
-		                	<li><a href="#">Arkiv</a></li>
-		                    <li><a href="index.html#nyheter">Kontakt</a></li>
-		                    <li><a href="dashboard/index.html">Dashboard</a></li>
-		                </ul>
-		            </nav>
-		        </div> <!-- .header-menu & header-menu__right -->
-			</div> <!-- .header-menu__container -->
-
-			</div>
-			<div class="header__text">
-					<div class="header__text--title fadeInRight">
-						<span>Volvo <i class="fa fa-heart" aria-hidden="true"></i> Eko</span>
-					</div>
-					<div class="header__text--text">
-						<span>Det är ett välkänt faktum att läsare</span>
-					</div>
-				</div>
-		</header>
-	</div>
 
 	<!-- Message -->
 
