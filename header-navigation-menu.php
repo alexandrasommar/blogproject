eabf1aeaeb932213ec3423dee1522045b6912112
Tobias Setterborg <setterborg.tobias@gmail.com>
2016-12-06 14:14:39 +0100

    Ändrar i navigationen.

--- header-navigation-menu.php
+++ header-navigation-menu.php
@@ -7,19 +7,17 @@
 
 				<!-- Header navigation left -->
 
-				<div class="header__menu header-menu__left">
-					<nav>
-						<ul>
-							<li><a href="#">Bilen</a></li>
-							<li><a href="#">Kampanjen</a></li>
-							<li><a href="category.php">Kategorier</a></li>
-						</ul>
-					</nav>
-				</div> <!-- .header-menu & header-menu__left -->
+				<nav class="header__menu">
+					<ul>
+						<li><a href="#">Bilen</a></li>
+						<li><a href="#">Kampanjen</a></li>
+						<li><a href="category.php">Kategorier</a></li>
+					</ul>
+				</nav> <!-- .header-menu -->
 
 				<!-- Header navigation center -->
 
-				<div class="header__menu header-menu__logo">
+				<div class="header__menu">
 		                <a href="index.php"><img class="logo-img" src="img/Volvo_Logos_Iron_Mark.png" alt="Volvo"></a>         
 				</div> <!-- .header-menu & header-menu__logo -->
 				
@@ -46,15 +44,13 @@
 		
 				<!-- Header navigation right -->
 
-				<div class="header__menu header-menu__right">
-		        	<nav>
-		                <ul>
-		                	<li><a href="#">Arkiv</a></li>
-		                    <li><a href="contact.php">Kontakt</a></li>
-		                    <li><a href="dashboard/index.php">Dashboard</a></li>
-		                </ul>
-		            </nav>
-		        </div> <!-- .header-menu & header-menu__right -->
+				<nav class="header__menu">
+	                <ul>
+	                	<li><a href="#">Arkiv</a></li>
+	                    <li><a href="contact.php">Kontakt</a></li>
+	                    <li><a href="dashboard/index.php">Dashboard</a></li>
+	                </ul>
+		        </nav> <!-- .header-menu -->
 			</div> <!-- .header-menu__container -->
 
 		</nav> <!-- .header__nav -->
