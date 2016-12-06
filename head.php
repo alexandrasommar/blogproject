ce189bb858f0c942b32138245ac977b3168e65e0
Tobias Setterborg <setterborg.tobias@gmail.com>
2016-12-06 14:35:53 +0100

    head.php

---
+++ head.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Volvo loves eco</title>
+	<!-- Regular CSS -->
+	<link rel="stylesheet" href="css/style.css">
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
+</head>
+
+<body>
