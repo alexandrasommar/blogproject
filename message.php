43979594c2bcbcc46419242d691e1317a35006c9
Tobias Setterborg <setterborg.tobias@gmail.com>
2016-12-06 21:54:21 +0100

    Anpassade message.php till html5.

--- message.php
+++ message.php
@@ -1,6 +1,6 @@
-<div class="container">
-	<div class="message">
+<div class="message__container">
+	<section class="message">
 		<h3>We love eco</h3>
 		<p>Det är ett välkänt faktum att läsare distraheras av läsbar text på en.</p>
-	</div>
+	</section>
 </div>
