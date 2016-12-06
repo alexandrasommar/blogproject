5dc8ecd331f4378189a43c0ff63608cba09377e2
Tobias Setterborg <setterborg.tobias@gmail.com>
2016-12-06 15:05:27 +0100

    Lade in include footer.php.

--- author.php
+++ author.php
@@ -109,29 +109,4 @@
 
 	<!-- Footer -->
 
-	<div class="footer-container">
-		<div class="nav">
-			<ul class="ul-nav">
-				<li><a href="#">About</a></li>
-				<li><a href="#">People</a></li>
-				<li><a href="#">Archive</a></li>
-				<li><a href="#">Volvo</a></li>
-			</ul>
-		</div>
-		<div class="nav-social-icon">
-			<ul class="ul-social">
-				<li><a href="#"><img class="social-icon" src="img/facebook.svg" alt="Facebook icon"></a></li>
-				<li><a href="#"><a href="#"><img class="social-icon" src="img/youtube.svg" alt="Youtube icon"</a></li>
-				<li><a href="#"><a href="#"><img class="social-icon" src="img/instagram.svg" alt="Instagram icon"</a></li>
-				<li><a href="#"><a href="#"><img class="social-icon" src="img/twitter.svg" alt="Twitter icon"</a></li>
-			</ul>
-		</div>
-		<div class="volvo-logo"><img class="volvo-icon" src="img/volvo-logo.svg" alt="Volvo logo"></div>
-	</div>
-
-
-
-
-	<script src="https://use.fontawesome.com/78a857f410.js"></script>
-</body>
-</html>
+<?php include "footer.php"; ?>
--- category.php
+++ category.php
@@ -1,7 +1,10 @@
 <?php include "head.php"; ?>
+
+<div class="header__container--post">
+
 <?php include "header-navigation-menu.php"; ?>
 	
-	
+</div>
 
 	<!-- Message -->
 	
@@ -42,29 +45,4 @@
 
 	<!-- Footer -->
 
-	<div class="footer-container">
-		<div class="nav">
-			<ul class="ul-nav">
-				<li><a href="#">About</a></li>
-				<li><a href="#">People</a></li>
-				<li><a href="#">Archive</a></li>
-				<li><a href="#">Volvo</a></li>
-			</ul>
-		</div>
-		<div class="nav-social-icon">
-			<ul class="ul-social">
-				<li><a href="#"><img class="social-icon" src="img/facebook.svg" alt="Facebook icon"></a></li>
-				<li><a href="#"><a href="#"><img class="social-icon" src="img/youtube.svg" alt="Youtube icon"</a></li>
-				<li><a href="#"><a href="#"><img class="social-icon" src="img/instagram.svg" alt="Instagram icon"</a></li>
-				<li><a href="#"><a href="#"><img class="social-icon" src="img/twitter.svg" alt="Twitter icon"</a></li>
-			</ul>
-		</div>
-		<div class="volvo-logo"><img class="volvo-icon" src="img/volvo-logo.svg" alt="Volvo logo"></div>
-	</div>
-
-
-
-
-	<script src="https://use.fontawesome.com/78a857f410.js"></script>
-</body>
-</html>
+<?php include "footer.php"; ?>
