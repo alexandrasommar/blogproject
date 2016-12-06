fe9ff2e1addca474fe4d6addfa9c6004f0bfa6e5
Alexandra Sommar <alexandra@sommar.se>
2016-12-06 13:26:29 +0100

    Lade till ; efter include, samt lagt till db-uppkopplingen i category.php

--- category.php
+++ category.php
@@ -1,3 +1,5 @@
+<?php require "include/db.php"; ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- index.php
+++ index.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Volvo loves eco</title>
-	<!-- Regular CSS -->
-	<link rel="stylesheet" href="css/style.css">
-	<!-- Google Fonts -->
-	<link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
-</head>
 
-<?php include "header-navigation-menu.php" ?>
-<?php include "header.php" ?>
+
+<?php include "header-navigation-menu.php"; ?>
+<?php include "header.php"; ?>
 	
 
 
