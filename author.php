84e81a5c045ecdf07907684e369e924d4569e7ba
Tobias Setterborg <setterborg.tobias@gmail.com>
2016-12-07 22:19:33 +0100

    Småfix

--- author.php
+++ author.php
@@ -26,37 +26,39 @@
 
 		<section class="post">
 			<article class="post__article">
-				<div class="post__img"><a href="post.php"><img class="post__img--styling" src="img/volvo_bg_1.jpg" alt="Bil"></a></div>
+				<div class="post__img">
+					<a href="post.php"><img class="post__img--styling" src="img/volvo_bg_1.jpg" alt="Bil"></a>
+				</div> <!-- .post__img -->
 				<div class="post__text">
 					<h2><a href="post.php">Lorem ipsum</a></h2>
 					<span>Av: <span class="author"><a href="author.html">Johan Walberg</a></span>, 14 nov 2016</span>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis eos repellat nihil sed explicabo labore, mollitia provident optio sit eveniet.</p>
-				</div>
-			</article>
+				</div> <!-- .post__text -->
+			</article> <!-- .post__article -->
 
 			<article class="post__article">
-				<div class="post__img"><a href="post.php"><img class="post__img--styling" src="img/volvo_bg_1.jpg" alt="Bil"></a></div>
+				<div class="post__img">
+					<a href="post.php"><img class="post__img--styling" src="img/volvo_bg_1.jpg" alt="Bil"></a>
+				</div> <!-- .post__img -->
 				<div class="post__text">
 					<h2><a href="post.php">Lorem ipsum</a></h2>
 					<span>Av: <span class="author"><a href="author.html">Johan Walberg</a></span>, 14 nov 2016</span>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis eos repellat nihil sed explicabo labore, mollitia provident optio sit eveniet.</p>
-				</div>
-			</article>
+				</div> <!-- .post__text -->
+			</article> <!-- .post__article -->
 
 			<article class="post__article">
-				<div class="post__img"><a href="post.php"><img class="post__img--styling" src="img/volvo_bg_1.jpg" alt="Bil"></a></div>
+				<div class="post__img">
+					<a href="post.php"><img class="post__img--styling" src="img/volvo_bg_1.jpg" alt="Bil"></a>
+				</div> <!-- .post__img -->
 				<div class="post__text">
 					<h2><a href="post.php">Lorem ipsum</a></h2>
 					<span>Av: <span class="author"><a href="author.html">Johan Walberg</a></span>, 14 nov 2016</span>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis eos repellat nihil sed explicabo labore, mollitia provident optio sit eveniet.</p>
-				</div>
-			</article>
-		</section>
+				</div> <!-- .post__text -->
+			</article> <!-- .post__article -->
+		</section> <!-- .post -->
 	</div>
-
-	
-
-	
 
 	<!-- Footer -->
 
