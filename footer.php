8ae50916ba242052294cea23d4662bc9c889adec
Tobias Setterborg <setterborg.tobias@gmail.com>
2016-12-07 13:40:57 +0100

    Anpassade footer.php till html5-standard.

--- footer.php
+++ footer.php
@@ -1,16 +1,16 @@
 <!-- Footer -->
 
-	<footer class="footer-container">
-		<div class="nav">
-			<ul class="ul-nav">
+	<footer class="footer__container">
+		<nav class="footer__nav">
+			<ul>
 				<li><a href="#">About</a></li>
 				<li><a href="#">People</a></li>
 				<li><a href="#">Archive</a></li>
 				<li><a href="#">Volvo</a></li>
 			</ul>
-		</div>
-		<div class="nav-social-icon">
-			<ul class="ul-social">
+		</nav>
+		<div class="footer__nav">
+			<ul>
 				<li><a href="#"><img class="social-icon" src="img/facebook.svg" alt="Facebook icon"></a></li>
 				<li><a href="#"><a href="#"><img class="social-icon" src="img/youtube.svg" alt="Youtube icon"</a></li>
 				<li><a href="#"><a href="#"><img class="social-icon" src="img/instagram.svg" alt="Instagram icon"</a></li>
