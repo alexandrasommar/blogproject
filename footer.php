d62f5947c1d419074439d7720f0d6ce8031690fb
Tobias Setterborg <setterborg.tobias@gmail.com>
2016-12-07 20:15:29 +0100

    Ändrade lite i footer.php (kommentarer och anpassade klassnamn till BEM)

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <!-- Footer -->
 
-	<footer class="footer__container">
+	<footer class="footer">
 		<nav class="footer__nav">
 			<ul>
 				<li><a href="#">About</a></li>
@@ -8,21 +8,20 @@
 				<li><a href="#">Archive</a></li>
 				<li><a href="#">Volvo</a></li>
 			</ul>
-		</nav>
+		</nav> <!-- .footer__nav -->
 		<nav class="footer__nav">
 			<ul>
 				<li><a href="#"><img class="social-icon" src="img/facebook.svg" alt="Facebook icon"></a></li>
-				<li><a href="#"><a href="#"><img class="social-icon" src="img/youtube.svg" alt="Youtube icon"</a></li>
-				<li><a href="#"><a href="#"><img class="social-icon" src="img/instagram.svg" alt="Instagram icon"</a></li>
-				<li><a href="#"><a href="#"><img class="social-icon" src="img/twitter.svg" alt="Twitter icon"</a></li>
+				<li><a href="#"><img class="social-icon" src="img/youtube.svg" alt="Youtube icon"</a></li>
+				<li><a href="#"><img class="social-icon" src="img/instagram.svg" alt="Instagram icon"</a></li>
+				<li><a href="#"><img class="social-icon" src="img/twitter.svg" alt="Twitter icon"</a></li>
 			</ul>
-		</nav>
-		<div class="volvo-logo"><img class="volvo-icon" src="img/volvo-logo.svg" alt="Volvo logo"></div>
-	</footer>
-
-
-
-
+		</nav> <!-- .footer__nav -->
+		<div class="footer__logo">
+			<img class="volvo-icon" src="img/volvo-logo.svg" alt="Volvo logo">
+		</div> <!-- .footer__logo -->
+	</footer> <!-- .footer__ -->
+	<!-- Fontawesome -->
 	<script src="https://use.fontawesome.com/78a857f410.js"></script>
 </body>
 </html>
