81e2660dfd000c28948da461e08d0c8e321d7d02
Tobias Setterborg <setterborg.tobias@gmail.com>
2016-12-07 22:04:35 +0100

    Mer anpassningar till BEM.

--- author.php
+++ author.php
@@ -1,52 +1,57 @@
 <?php include "head.php"; ?>
 
 	<!-- Header -->
-<div class="header__container--post">
-<?php include "header-navigation-menu.php"; ?>
-</div>
-	
 
-	<!-- Message -->
+<div class="blog-post__image">
+
+<?php include "header-navigation-menu.php"; ?>
+
+</div> <!-- .blog-post__image -->
 	
-	<div class="catagory-container">
-		<div class="author-profile">
-			<div class="author-user-image">
+	<div class="category__container">
+
+	<!-- Author information box -->
+
+		<div class="author-information-box">
+			<div class="author-information-box__image">
 				<img src="img/user-image.jpg" alt="">
-			</div>
-			<div class="author-user-text">
+			</div> <!-- .author-information-box__image -->
+			<div class="author-information-box__text">
 				<h3>Gunther Beard</h3>
 				<p>Det är ett välkänt faktum att läsare distraheras av läsbar text på en sida när man skall studera layouten. Poängen med Lorem Ipsum är att det ger ett normalt ordflöde.</p>
-			</div>
-		</div>
+			</div> <!-- .author-information-box__text -->
+		</div> <!-- .author-information-box -->
+		
 		<!-- Posts -->
-	<div class="post-container">
-		<div class="post">
-			<div class="post__img"><a href="post.html"><img class="post__img--picture" src="img/volvo_bg_1.jpg" alt="Bil"></a></div>
-			<div class="post-text">
-				<h2><a href="post.html">Lorem ipsum</a></h2>
-				<span>Av: <span class="author"><a href="author.html">Johan Walberg</a></span>, 14 nov 2016</span>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis eos repellat nihil sed explicabo labore, mollitia provident optio sit eveniet.</p>
-			</div>
-		</div>
 
-		<div class="post">
-			<div class="post__img"><a href="post.html"><img class="post__img--picture" src="img/volvo_bg_1.jpg" alt="Bil"></a></div>
-			<div class="post-text">
-				<h2><a href="post.html">Lorem ipsum</a></h2>
-				<span>Av: <span class="author"><a href="author.html">Johan Walberg</a></span>, 14 nov 2016</span>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis eos repellat nihil sed explicabo labore, mollitia provident optio sit eveniet.</p>
-			</div>
-		</div>
+		<section class="post">
+			<article class="post__article">
+				<div class="post__img"><a href="post.php"><img class="post__img--styling" src="img/volvo_bg_1.jpg" alt="Bil"></a></div>
+				<div class="post__text">
+					<h2><a href="post.php">Lorem ipsum</a></h2>
+					<span>Av: <span class="author"><a href="author.html">Johan Walberg</a></span>, 14 nov 2016</span>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis eos repellat nihil sed explicabo labore, mollitia provident optio sit eveniet.</p>
+				</div>
+			</article>
 
-		<div class="post">
-			<div class="post__img"><a href="post.html"><img class="post__img--picture" src="img/volvo_bg_1.jpg" alt="Bil"></a></div>
-			<div class="post-text">
-				<h2><a href="post.html">Lorem ipsum</a></h2>
-				<span>Av: <span class="author"><a href="author.html">Johan Walberg</a></span>, 14 nov 2016</span>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis eos repellat nihil sed explicabo labore, mollitia provident optio sit eveniet.</p>
-			</div>
-		</div>
-	</div>
+			<article class="post__article">
+				<div class="post__img"><a href="post.php"><img class="post__img--styling" src="img/volvo_bg_1.jpg" alt="Bil"></a></div>
+				<div class="post__text">
+					<h2><a href="post.php">Lorem ipsum</a></h2>
+					<span>Av: <span class="author"><a href="author.html">Johan Walberg</a></span>, 14 nov 2016</span>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis eos repellat nihil sed explicabo labore, mollitia provident optio sit eveniet.</p>
+				</div>
+			</article>
+
+			<article class="post__article">
+				<div class="post__img"><a href="post.php"><img class="post__img--styling" src="img/volvo_bg_1.jpg" alt="Bil"></a></div>
+				<div class="post__text">
+					<h2><a href="post.php">Lorem ipsum</a></h2>
+					<span>Av: <span class="author"><a href="author.html">Johan Walberg</a></span>, 14 nov 2016</span>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis eos repellat nihil sed explicabo labore, mollitia provident optio sit eveniet.</p>
+				</div>
+			</article>
+		</section>
 	</div>
 
 	
