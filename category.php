dc47a2e99ad713373712dd87e8d2d133ebe58d74
Alexandra Sommar <alexandra@sommar.se>
2016-12-07 13:24:03 +0100

    Visar inläggen på både vänster och höger sida på category-sidan, dock i dubletter..

--- category.php
+++ category.php
@@ -15,35 +15,74 @@
 $category = $_GET['cat'];
 
 $query = "SELECT * FROM posts WHERE post_category_id = {$category}";
+if ($result = mysqli_query($conn, $query)) { 
+		$row_cnt = mysqli_num_rows($result);
+	}
+		mysqli_free_result($result);
+
 
 if($stmt->prepare($query)) {
 
 		$stmt->execute();
 		$stmt->bind_result($post_id, $category_id, $post_title, $post_author, $post_date, $post_image, $post_content, $post_status);
 
-		while(mysqli_stmt_fetch($stmt)) {
-
-			?>
-			<div class="category-headline">
-			<h3><?php echo $category;  ?></h3>
-		</div>
 		
+		
+		while(mysqli_stmt_fetch($stmt)) { ?>
 
-		<div class="post-category">
-			<div class="post-category-img">
-				<div class="post__img"><a href="post.html"><img class="post__img--picture" src="img/volvo_bg_1.jpg" alt="Bil"></a></div>
-			</div>
-			<div class="post-category-text">
-				<div class="post-text">
-				<h2><a href="post.html"><?php echo $post_title;?></a></h2>
-				<span class="byline-category">Av: <span class="author"><?php echo $post_author; ?></span>,<?php echo $post_date ?></span>
-				<p><?php echo $post_content ?></p>
-			</div>
-			</div>
-		</div>
+
 			<?php
+			
+			for ($i=0; $i < $row_cnt; $i++) { 
 
+			if ($i & 1) { 
+
+				?>
+			<div class="post-category">
+				<div class="post-category-img">
+					<div class="post__img"><a href="post.html"><img class="post__img--picture" src="img/volvo_bg_1.jpg" alt="Bil"></a></div>
+				</div>
+				<div class="post-category-text">
+					<div class="post-text">
+						<h2><a href="post.html"><?php echo $post_title;?></a></h2>
+						<span class="byline-category">Av: <span class="author"><?php echo $post_author; ?></span>,<?php echo $post_date; ?></span>
+						<p><?php echo $post_content; ?></p>
+					</div>
+				</div>
+			</div>
+			<?php
+			
+			}elseif(!$i & 1) { ?>
+
+			<div class="post-category">
+				<div class="post-category-text">
+					<div class="post-text post-text-category">
+						<h2><a href="post.html"><?php echo $post_title; ?></a></h2>
+						<span class="byline-category">Av: <span class="author"><?php echo $post_author; ?></span>, <?php echo $post_date; ?></span>
+						<p><?php echo $post_content; ?></p>
+					</div>
+				</div>
+				<div class="post-category-img">
+					<div class="post__img"><a href="post.html"><img class="post__img--picture" src="img/volvo_bg_1.jpg" alt="Bil"></a>
+					</div>
+				</div>
+			</div>
+
+
+			
+			<?php
+			
+				}
+			
+		
 		}
+		
+		}
+		?>
+	<div class="category-headline">
+			<h3><?php echo $category;  ?></h3>
+			</div>
+		<?php
 }
 
 
