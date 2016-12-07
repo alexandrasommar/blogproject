766e5b24f8ce46200b010abe6fb93e41f1382fb0
Tobias Setterborg <setterborg.tobias@gmail.com>
2016-12-07 22:28:25 +0100

    Lade till kommentarer och tog bort login.html.

--- header.php
+++ header.php
@@ -2,10 +2,10 @@
 				<section class="header__text">
 					<div class="header__text--title">
 						<span>Volvo <i class="fa fa-heart" aria-hidden="true"></i> Eko</span>
-					</div>
+					</div> <!-- .header__text--title -->
 					<div class="header__text--text">
 						<span>Det är ett välkänt faktum att läsare</span>
-					</div>
+					</div> <!-- .header__text--text -->
 				</section> <!-- .header__text -->
 		</header>
 	</div>
