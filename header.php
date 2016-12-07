c97f7b8fcc23a76329c887f07171d03f9be5fd39
Tobias Setterborg <setterborg.tobias@gmail.com>
2016-12-07 10:42:26 +0100

    Html5-anpassning i hedern.

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 	
-				<div class="header__text">
-					<div class="header__text--title fadeInRight">
+				<section class="header__text">
+					<div class="header__text--title">
 						<span>Volvo <i class="fa fa-heart" aria-hidden="true"></i> Eko</span>
 					</div>
 					<div class="header__text--text">
 						<span>Det är ett välkänt faktum att läsare</span>
 					</div>
-				</div> <!-- .header__text -->
+				</section> <!-- .header__text -->
 		</header>
 	</div>
