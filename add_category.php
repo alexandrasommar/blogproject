735ebbc80897616e75df9c2a04a85a0c0dfd24c8
Tobias Setterborg <setterborg.tobias@gmail.com>
2016-12-08 17:17:22 +0100

    Formulär för add_category.php

---
+++ add_category.php
@@ -0,0 +1,16 @@
+<?php include "head.php"; ?>
+
+<form action="" method="post">
+	<div class="form-group">
+		<label for="title">Lägg till kategori</label>
+		<input type="text" class="form-control" name="title" placeholder="hej">
+	</div>
+
+
+	<div class="form-group">
+		<input class="btn btn-primary" type="submit" name="create_post" value="Publish Post"></textarea>
+	</div>
+
+
+
+</form>
---
+++ dashboard/add_category.php
@@ -0,0 +1,12 @@
+<?php include "../head.php"; ?>
+<section class="form">
+<form action="" method="post">
+	<div class="form__input">
+		<label for="title">Lägg till ny kategori</label>
+		<input type="text" class="form-control" name="title">
+	</div>
+	<div class="form__input">
+		<input class="btn btn-primary" type="submit" name="create_post" value="Lägg till"></textarea>
+	</div>
+</form>
+</section>
