7022ec55d52d1b188e9fdb46221d7f5624d62f05
Tobias Setterborg <setterborg.tobias@gmail.com>
2016-12-08 11:30:30 +0100

    Lade till form-taggar.

--- login.php
+++ login.php
@@ -6,10 +6,12 @@
 				<img src="img/Volvo_Ocean_Race_Edition.jpg" alt="">
 				<div class="login__form">
 					<div class="login__input">
-						<span>Logga in</span>
-						<input type="text" name="username" placeholder="Användarnamn">
-						<input type="text" name="password" placeholder="Lösenord">
-						<button>Logga in</button>
+						<form action="">
+							<span>Logga in</span>
+							<input type="text" name="username" placeholder="Användarnamn">
+							<input type="text" name="password" placeholder="Lösenord">
+							<button>Logga in</button>
+						</form>
 					</div> <!-- .login__input -->
 				</div> <!-- .login__form -->
 			</div> <!-- .login__image -->
