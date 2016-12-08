7de2386d1a280abe9a054a71206603baa8447a12
Alexandra Sommar <alexandra@sommar.se>
2016-12-08 10:00:18 +0100

    Tog bort dubletterna, nu är alla inlägg på samma sida.

--- category.php
+++ category.php
@@ -9,16 +9,15 @@
 
 
 <div class="catagory-container">
-
+<div class="category-headline">
+			<h3><?php echo $cat_name;  ?></h3>
+			</div>
 <?php 
 
 $category = $_GET['cat'];
 
 $query = "SELECT posts.*, categories.cat_name FROM posts LEFT JOIN categories ON posts.post_category_id = categories.cat_id WHERE post_category_id = {$category} AND post_status = 1";
-if ($result = mysqli_query($conn, $query)) { 
-		$row_cnt = mysqli_num_rows($result);
-	}
-		mysqli_free_result($result);
+
 
 
 if($stmt->prepare($query)) {
@@ -30,14 +29,6 @@ if($stmt->prepare($query)) {
 		
 		while(mysqli_stmt_fetch($stmt)) { ?>
 
-
-			<?php
-			
-			
-			for ($i=0; $i < $row_cnt; $i++) { 
-				if($i & 1) {
-
-				?>
 			<div class="post-category">
 				<div class="post-category-img">
 					<div class="post__img"><a href="post.php?post=<?php echo $post_id; ?>"><img class="post__img--picture" src="img/volvo_bg_1.jpg" alt="Bil"></a></div>
@@ -50,38 +41,19 @@ if($stmt->prepare($query)) {
 					</div>
 				</div>
 			</div>
-			<?php
-		}elseif(!$i & 1) {
-			?>
-
-			<div class="post-category">
-				<div class="post-category-text">
-					<div class="post-text post-text-category">
-						<h2><a href="post.php?post=<?php echo $post_id; ?>"><?php echo $post_title; ?></a></h2>
-						<span class="byline-category">Av: <span class="author"><?php echo $post_author; ?></span>, <?php echo $post_date; ?></span>
-						<p><?php echo $post_content; ?></p>
-					</div>
-				</div>
-				<div class="post-category-img">
-					<div class="post__img"><a href="post.php?post=<?php echo $post_id; ?>"><img class="post__img--picture" src="img/volvo_bg_1.jpg" alt="Bil"></a>
-					</div>
-				</div>
-			</div>
+			
 
 
 			
 			<?php
-			}
-				}
+			
 			
 		
 		}
 		
 		
 		?>
-	<div class="category-headline">
-			<h3><?php echo $cat_name;  ?></h3>
-			</div>
+	
 		<?php
 }
 
