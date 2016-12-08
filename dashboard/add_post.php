f93a8cac9b4111be500f965b241dcbed6990b123
Tobias Setterborg <setterborg.tobias@gmail.com>
2016-12-08 17:37:16 +0100

    Lade till lite mer saker i formuläret.

--- dashboard/add_post.php
+++ dashboard/add_post.php
@@ -1,6 +1,6 @@
 <?php include "../head.php"; ?>
 <section class="form">
-<form action="" method="post">
+<form action="" method="post" enctype="multipart/form-data">
 	<div class="form__input">
 		<label for="title">Titel</label>
 		<input type="text" class="form-control" name="title">
@@ -8,6 +8,13 @@
 	<div class="form__input">
 		<label for="post_content">Post Content</label>
 		<textarea class="form-control" name="post_content" id="" cols="30" rows="10"></textarea>
+	</div>
+	<div class="form-group">
+		<label for="post_image">Post Image</label>
+		<input type="file" name="image">
+	</div>
+	<div class="form__input">
+		<select name="post_category" id=""></select>
 	</div>
 	<div class="form__input">
 		<input class="btn" type="submit" name="create_post" value="Publisera">
