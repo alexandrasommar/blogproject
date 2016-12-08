fce9328373637dadb333900d7fa4dd811703d15a
Alexandra Sommar <alexandra@sommar.se>
2016-12-08 19:26:31 +0100

    La till en funktionskommentar, samt lade till funktionen countComments, som räknar antal kommentarer ett inlägg har

--- include/functions.php
+++ include/functions.php
@@ -1,4 +1,17 @@
 <?php
+
+/*
+* The function counts how many posts there are per month.
+* The query checks the month and the year based on post_date 
+* from the posts table where the status is published.
+*
+* @param number $year is the year from post_date
+* @param number $month is the month from post_date
+* @param string $monthName converts the month number to month name
+*
+* If there are posts, the function returns an <option>. 
+*/
+
 function selectMonth () {
 	global $conn;
 	$query = "SELECT YEAR(post_date) AS YEAR, MONTH(post_date) AS MONTH, COUNT(*) AS TOTAL FROM posts WHERE post_status = 1 GROUP BY YEAR, MONTH DESC";
@@ -41,5 +54,19 @@ function showMonths () {
 	}
 
 	}
+}
+
+function countComments () {
+	global $conn;
+	$query = "SELECT * FROM comments WHERE comment_post_id = '{$_GET['post']}'";
+	if ($result = mysqli_query($conn, $query)) {
+		
+		$row_cnt = mysqli_num_rows($result);
+		echo $row_cnt;
+
+
+		}
+		
+   		mysqli_free_result($result);
 }
 ?>
