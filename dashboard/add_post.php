09b58bc56823ad4ebb5029c42247f47565571cad
Tobias Setterborg <setterborg.tobias@gmail.com>
2016-12-08 17:33:37 +0100

    Början på ett formulär för add_post.php.

---
+++ dashboard/add_post.php
@@ -0,0 +1,17 @@
+<?php include "../head.php"; ?>
+<section class="form">
+<form action="" method="post">
+	<div class="form__input">
+		<label for="title">Titel</label>
+		<input type="text" class="form-control" name="title">
+	</div>
+	<div class="form__input">
+		<label for="post_content">Post Content</label>
+		<textarea class="form-control" name="post_content" id="" cols="30" rows="10"></textarea>
+	</div>
+	<div class="form__input">
+		<input class="btn" type="submit" name="create_post" value="Publisera">
+		<input class="btn" type="submit" name="create_post" value="Spara">
+	</div>
+</form>
+</section>
