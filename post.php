17435ce44ef64d56bf3989e3fa6ed47e716e8050
Tobias Setterborg <setterborg.tobias@gmail.com>
2016-12-08 10:32:20 +0100

    no message

--- post.php
+++ post.php
@@ -40,12 +40,25 @@
 							<li class="accordion"><a href="#comments">Kommentera</a></li>
 							<div class="panel">
 								<div class="comments-box__form">
-									<form action="">
-										<input type="text" name="firstname" value="John">
-										<input type="text" name="firstname" value="John">
-										<input type="text" name="firstname" value="John">
-										<input type="text" name="firstname" value="John">
-									</form>
+																		<!-- comment post form -->
+								    <!-- todo: Kommentarsfält ska valideras med hjälp av regex. Primärt e-postadressen, dvs. kommentaren ska inte sättas in i databasen om man inte har skrivit en korrekt utformad e-postadress. -->
+
+								    <em>Fyll i alla fält för att publicera din kommentar.</em>
+								    <br> <!-- CSS-fix? Lite luft här, mellan "Tänk på"-texten och formuläret. Om det inte finns något för det i CSS:en så kanske en br duger.-->
+								    <form method="post" action=""> <!-- todo: action: mellan citationstecknen: Lägg in så att vid klick på Publicera-knappen så publiceras texten under "Läs kommentarer" och förslag: automatiskt blir "Visa alla inlägg" aktivt på samma sida (för att man ska se var ens egen kommentar hamnar). Kommentarsfälten ska då åter bli nollställda. Likaså ska det läggas in en publiceringstidpunkt ovanför kommentaren. --> 
+								        <p>Namn:</p>
+								        <input type="text" name="namn" placeholder="Förnamn Efternamn" autocomplete><br>
+								        <p>E-postadress:</p>
+								        <input type="email" name="e-postadress" autocomplete><br>  <!-- Vi ska använda regex för att validera e-postadressen -->
+								        <p>Din webbadress:</p>
+								        <input type="url" name="webbadress" placeholder="http://www.adressen.se" autocomplete><br>  <!-- Finns det ngt som gör att http:// eller https:// inte behöver skrivas av användaren? -->
+								        <br> <!-- CSS-fix hellre än br?: Ngt slags liten avgränsare eller luft här för att skilja kommentatör-uppgifterna från kommentarfältet. Det lyfter fram kommentarfältet på ett lagom sätt. -->
+								        <p>Din kommentar:</p>
+								        <textarea name="comment-text" rows="5" cols="40"></textarea><br> <!-- todo: CSS för textarea: border: 1px solid #EFEFEF -->
+								        <br>
+								        <input type="submit" value="Publicera"> <input type="submit" value="Avbryt">    <!-- Är det schysst att även ha en "Avbryt"-knapp som tömmer alla fält om man ångrar sig och inte vill kommentera? -->
+								    </form>
+
 								</div>
 							</div>
 							<li class="accordion"><a href="#comments">Läs Kommentarer (x)</a></li>
