30bd2acd251c7aa5697a467134793b8d61629439
Alexandra Sommar <alexandra@sommar.se>
2016-12-08 13:51:04 +0100

    Lade till author-länk

--- author-information-box.php
+++ author-information-box.php
@@ -3,7 +3,7 @@
 		<img src="<?php echo $image; ?>" alt="">
 	</div> <!-- .author-information-box__image -->
 	<div class="author-information-box__text">
-		<h3><author><?php echo "$firstname $lastname"; ?></author></h3>
+		<h3><author><a href="author.php?author=<?php echo $post_author_id; ?>"><?php echo "$firstname $lastname"; ?></author></a></h3>
 		<p><?php echo $description; ?></p>
 	</div> <!-- .author-information-box__text -->
 </div> <!-- .author-information-box -->
