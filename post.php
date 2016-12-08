aec2658d32f9b51677aef7b524a0d492c864df30
Johan Walberg <johanwalberg@outlook.com>
2016-12-08 15:01:10 +0100

    Fixa kommentarer så dem läggs in i databasen

--- post.php
+++ post.php
@@ -8,7 +8,40 @@
 
 
 </div> <!-- .blog-post__image -->
-<?php $post = $_GET["post"]; ?>
+<?php
+$post = $_GET["post"]; 
+
+if(isset($_POST['submit'])) {
+
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$website = $_POST['website'];
+	$content = $_POST['content'];
+
+
+
+
+	$query ="INSERT INTO comments(comment_post_id, comment_author, comment_date, comment_email, comment_content, comment_website) VALUES('{$post}', '{$name}', CURDATE(), '{$email}', '{$content}', '{$website}')";
+
+	if($stmt->prepare($query)) {
+
+		$stmt->execute(); 
+
+		
+
+
+	} else
+
+	die("query failed" . mysqli_error($conn));
+}
+
+
+
+
+ ?>
+
+
+
 <!-- Post -->
 	
 <section class="blog-post">
@@ -34,43 +67,9 @@ if($stmt->prepare($query)) {
 
 				<!-- Comment information -->
 
-				<div class="comments-box" id="comments">
-					<div class="comments">
-						<ul>
-							<li class="accordion"><a href="#comments">Kommentera</a></li>
-							<div class="panel">
-								<div class="comments-box__form">
-																		<!-- comment post form -->
-								    <!-- todo: Kommentarsfält ska valideras med hjälp av regex. Primärt e-postadressen, dvs. kommentaren ska inte sättas in i databasen om man inte har skrivit en korrekt utformad e-postadress. -->
-
-								    <em>Fyll i alla fält för att publicera din kommentar.</em>
-								    <br> <!-- CSS-fix? Lite luft här, mellan "Tänk på"-texten och formuläret. Om det inte finns något för det i CSS:en så kanske en br duger.-->
-								    <form method="post" action=""> <!-- todo: action: mellan citationstecknen: Lägg in så att vid klick på Publicera-knappen så publiceras texten under "Läs kommentarer" och förslag: automatiskt blir "Visa alla inlägg" aktivt på samma sida (för att man ska se var ens egen kommentar hamnar). Kommentarsfälten ska då åter bli nollställda. Likaså ska det läggas in en publiceringstidpunkt ovanför kommentaren. --> 
-								        <p>Namn:</p>
-								        <input type="text" name="namn" placeholder="Förnamn Efternamn" autocomplete><br>
-								        <p>E-postadress:</p>
-								        <input type="email" name="e-postadress" autocomplete><br>  <!-- Vi ska använda regex för att validera e-postadressen -->
-								        <p>Din webbadress:</p>
-								        <input type="url" name="webbadress" placeholder="http://www.adressen.se" autocomplete><br>  <!-- Finns det ngt som gör att http:// eller https:// inte behöver skrivas av användaren? -->
-								        <br> <!-- CSS-fix hellre än br?: Ngt slags liten avgränsare eller luft här för att skilja kommentatör-uppgifterna från kommentarfältet. Det lyfter fram kommentarfältet på ett lagom sätt. -->
-								        <p>Din kommentar:</p>
-								        <textarea name="comment-text" rows="5" cols="40"></textarea><br> <!-- todo: CSS för textarea: border: 1px solid #EFEFEF -->
-								        <br>
-								        <input type="submit" value="Publicera"> <input type="submit" value="Avbryt">    <!-- Är det schysst att även ha en "Avbryt"-knapp som tömmer alla fält om man ångrar sig och inte vill kommentera? -->
-								    </form>
-
-								</div>
-							</div>
-							<li class="accordion"><a href="#comments">Läs Kommentarer (x)</a></li>
-							<div class="panel">
-								<ul class="aside__nav--visible">
-									<li>Visa alla inlägg</li>
-									<li>Skriv nytt inlägg</li>
-								</ul>
-							</div>
-						</ul>
-					</div>
-				</div>
+				
+					
+				
 		</article> <!-- .blog-post__article -->			
 
 
@@ -83,9 +82,35 @@ if($stmt->prepare($query)) {
 
 
 ?>
+
+
 	
 
 	</section> <!-- .blog-post -->
+				<section class="blog-post">
+					<div class="comments">
+						<form method="post" action="">
+						<label for="name">Namn</label>
+						<input type="text" name="name" >
+
+						<label for="email">Email</label>
+						<input type="email" name="email" >
+
+						<label for="website">Hemsida</label>
+						<input type="text" name="website" >
+
+						<label for="content">Inlägg</label>
+						<textarea name="content"></textarea>
+						<input type="submit" name="submit" value="Kommentera">
+						</form>	
+
+
+
+					</div>
+</section>
+
+
+
 
 	
 	
