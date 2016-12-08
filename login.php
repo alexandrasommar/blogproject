22efdbf4657a3082d81d022dbb26d95f5d798466
Tobias Setterborg <setterborg.tobias@gmail.com>
2016-12-08 11:31:55 +0100

    Korrigerade lite fel i login-formuläret.

--- login.php
+++ login.php
@@ -5,14 +5,12 @@
 			<div class="login__image">
 				<img src="img/Volvo_Ocean_Race_Edition.jpg" alt="">
 				<div class="login__form">
-					<div class="login__input">
-						<form action="">
+					<form class="login__input">
 							<span>Logga in</span>
 							<input type="text" name="username" placeholder="Användarnamn">
 							<input type="text" name="password" placeholder="Lösenord">
 							<button>Logga in</button>
-						</form>
-					</div> <!-- .login__input -->
+					</form> <!-- .login__input -->
 				</div> <!-- .login__form -->
 			</div> <!-- .login__image -->
 		</div> <!-- .login__box -->
