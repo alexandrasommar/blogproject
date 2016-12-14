bd06d2be4dd1b5d2dbb79b880073c33ee4abcec2
Alexandra Sommar <alexandra@sommar.se>
2016-12-14 14:09:08 +0100

    Visar statistik på startsidan

--- dashboard/index.php
+++ dashboard/index.php
@@ -6,6 +6,48 @@
 	<div class="container">
 			<?php include "user_navigation.php"; ?>
 				<main>
+					<?php
+					$query = "SELECT * FROM posts ";
+					if($_SESSION['role'] != 'admin') {
+						$query .= "WHERE post_author_id = '{$_SESSION['user_id']}'";
+					}
+					if($result = mysqli_query($conn, $query)) {
+						$post_cnt = mysqli_num_rows($result);
+					}
+					$query = "SELECT comments.*, posts.post_author_id FROM comments LEFT JOIN posts ON posts.post_id = comments.comment_post_id ";
+					if($_SESSION['role'] != 'admin') {
+						 $query .= "WHERE post_author_id = '{$_SESSION['user_id']}'";
+						}
+					if($result = mysqli_query($conn, $query)) {
+						$comm_cnt = mysqli_num_rows($result);
+						if($post_cnt > 0) {
+						$average = $comm_cnt / $post_cnt;
+						}
+					}
+
+
+					?>
+					<?php
+							if($post_cnt == 0) {
+								echo "Tyvärr har du inga inlägg än, så ingen statistik kan visas. <a href='write_post.php'>Skriv inlägg här</a>";
+							} else {
+
+
+							?>
+					<table>
+						<tr>
+							<th>Antal inlägg</th>
+							<th>Antal kommentarer totalt</th>
+							<th>Antal kommentarer i snitt per inlägg</th>
+						</tr>
+						<tr>
+							
+							<td><?php echo $post_cnt; ?></td>
+							<td><?php echo $comm_cnt; ?></td>
+							<td><?php echo $average; ?></td>
+							<?php } ?>
+						</tr>
+					</table>
 					
 				</main>
 			</div>
