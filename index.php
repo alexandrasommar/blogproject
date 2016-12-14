f3a65697573124a96d383ea65c93fdd051d1ecb2
Alexandra Sommar <alexandra@sommar.se>
2016-12-14 14:32:59 +0100

    Lägger till paginering

--- index.php
+++ index.php
@@ -18,8 +18,31 @@
 	<section class="post">
 		<div class="post__container">
 	
-	<?php 
-	$query = "SELECT posts.*, categories.* FROM posts LEFT JOIN categories ON posts.post_category_id = categories.cat_id WHERE post_status = 1 ORDER BY post_date DESC";
+	<?php
+	$per_page = 6;
+	if(isset($_GET["page"])) {
+		$page = $_GET["page"];
+	} else {
+		$page = "";
+	}
+
+	if($page == "" || $page == 1) {
+		$page_1 = 0;
+	} else {
+		$page_1 = ($page * $per_page) - $per_page;
+	}
+
+	$post_query_count = "SELECT * FROM posts";
+	$find_count = mysqli_query($conn, $post_query_count);
+	$count = mysqli_num_rows($find_count);
+
+	$count = ceil($count / $per_page);
+
+
+
+
+
+	$query = "SELECT posts.*, categories.* FROM posts LEFT JOIN categories ON posts.post_category_id = categories.cat_id WHERE post_status = 1 ORDER BY post_date DESC LIMIT $page_1, $per_page";
 
 	if($stmt->prepare($query)) {
 
@@ -77,6 +100,15 @@
 
 		</div>
 	</section>
+	<?php
+
+	for ($i = 1; $i <= $count; $i++) {
+
+		echo "<a href='index.php?page={$i}'>{$i}</a>";
+
+	}
+
+	?>
 
 	<!-- Footer -->
 
