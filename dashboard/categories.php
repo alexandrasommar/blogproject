c7bdb266061307ff96f05ace35519edf266853a2
Alexandra Sommar <alexandra@sommar.se>
2016-12-17 11:39:56 +0100

    intabbning osv

--- dashboard/categories.php
+++ dashboard/categories.php
@@ -14,76 +14,65 @@ if(isset($_GET['delete'])) {
 	    }
 }
 ?>
+	<div class="container">
+		<?php include "user_navigation.php"; ?>
+		<main>
+			<!-- Display all categories -->
+			<div class="divTable">
+				<div class="divTableBody">
+					<div class="divTableRow divTableRow--header">
+						<div class="divTableCell">Kategorinamn</div>
+						<div class="divTableCell">Redigera</div>
+						<div class="divTableCell">Radera</div>
+					</div> <!-- .divTableRow header -->	
+					<?php
+					$query = "SELECT * FROM categories";
+					if($stmt->prepare($query)) {
+						$stmt->execute();
+						$stmt->bind_result($cat_id, $cat_name);
+						while(mysqli_stmt_fetch($stmt)) { ?>
+					<div class="divTableRow">
+						<div class="divTableCell"><?php echo $cat_name; ?></div>
+						<div class="divTableCell"><?php echo "<a href='categories.php?source=edit&cat=$cat_id'><i class='fa fa-pencil-square-o' aria-hidden='true'></i></a>"; ?></div>
+						<div class="divTableCell"><?php echo "<a href='categories.php?delete=$cat_id'><i class='fa fa-trash-o' aria-hidden='true'></i></a>"; ?></div>
+					</div> <!-- .divTableRow -->
+					<?php
+						}
+					}
 
-
-<div class="container">
-	<?php include "user_navigation.php"; ?>
-	<main>
-		
-
-		
-
-		<div class="divTable">
-			<div class="divTableBody">
-				<div class="divTableRow divTableRow--header">
-					<div class="divTableCell">Kategorinamn</div>
-					<div class="divTableCell">Redigera</div>
-					<div class="divTableCell">Radera</div>
-				</div>	
-		<?php
-
-		$query = "SELECT * FROM categories";
-		if($stmt->prepare($query)) {
-			$stmt->execute();
-			$stmt->bind_result($cat_id, $cat_name);
-			while(mysqli_stmt_fetch($stmt)) { ?>
-				<div class="divTableRow">
-					<div class="divTableCell"><?php echo $cat_name; ?></div>
-					<div class="divTableCell"><?php echo "<a href='categories.php?source=edit&cat=$cat_id'><i class='fa fa-pencil-square-o' aria-hidden='true'></i></a>"; ?></div>
-					<div class="divTableCell"><?php echo "<a href='categories.php?delete=$cat_id'><i class='fa fa-trash-o' aria-hidden='true'></i></a>"; ?></div>
+					?>	
 				</div>
-
-		<?php
-			}
-		}
-
-
-		?>	
+			</div> <!-- .divTable -->
+			<!-- Add Category -->
+			<div class="ad-category">
+				<a href="categories.php?source=add">Lägg till ny kategori</a>
 			</div>
-		</div>
-				<div class="ad-category">
-					<a href="categories.php?source=add">Lägg till ny kategori</a>
-				</div>
-		<?php
-		if(isset($message)) {
-			echo $message;
-		}	
-		?>
-<?php
+			<?php
+			if(isset($message)) {
+				echo $message;
+			}	
 
-if(isset($_GET['source'])) {
-	$source = $_GET['source'];
-	} else {
-	    $source = '';
-	}
+			if(isset($_GET['source'])) {
+				$source = $_GET['source'];
+				} else {
+				    $source = '';
+				}
 
-	switch ($source) {
-	    case 'edit':
-	        include "include/edit_category.php";
-	        break;
-	    case 'add':
-	        include "include/add_category.php";
-	        break;    
-	    }
-	 
-	
-?>
-
-	</main>
-</div>
-<!-- FontAwesom -->
-		<script src="https://use.fontawesome.com/78a857f410.js"></script>
-		<!-- JavaScript -->
-		<script src="script.js"></script>
+				switch ($source) {
+				    case 'edit':
+				        include "include/edit_category.php";
+				        break;
+				    case 'add':
+				        include "include/add_category.php";
+				        break;    
+				    }
+				
+			?>
+		</main>
+	</div> <!-- .container -->
+	<!-- FontAwesom -->
+	<script src="https://use.fontawesome.com/78a857f410.js"></script>
+	<!-- JavaScript -->
+	<script src="script.js"></script>
 </body>
 </html>
