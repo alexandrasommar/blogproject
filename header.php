7647e0502d1de33e50ed2c9166bf091ad397c655
AnnaStark <anna385stark@gmail.com>
2016-12-19 09:41:14 +0100

    Har uppdaterat payoffen i header.php och lagt in inledningstext i message.php.

--- header.php
+++ header.php
@@ -1,10 +1,10 @@
 	
 				<section class="header__text">
 					<div class="header__text--title">
-						<span>Volvo <i class="fa fa-heart" aria-hidden="true"></i> Eko</span>
+						<span>Volvo <i class="fa fa-heart" aria-hidden="true"></i> Eco</span>
 					</div> <!-- .header__text--title -->
 					<div class="header__text--text">
-						<span>Det är ett välkänt faktum att läsare</span>
+						<span>Omtanke om vår livsmiljö.</span>
 					</div> <!-- .header__text--text -->
 				</section> <!-- .header__text -->
 		</header>
--- message.php
+++ message.php
@@ -1,6 +1,6 @@
 <div class="message__container">
 	<section class="message">
 		<h3>We love eco</h3>
-		<p>Det är ett välkänt faktum att läsare distraheras av läsbar text på en.</p>
+		<p>Om du också vill bidra till ett bättre samhälle, både för människor och vår planet så är Eco-serien för dig. I den har vi byggt in den senaste tekniken för en så miljöstödjande bil som möjligt – för dig och för alla. Och självklart har vi också tänkt på miljöpåverkan under tillverkningen.</p>
 	</section>
 </div>
