b280f4fc7d04fb15c0da312fdcdbf2043256671f
Alexandra Sommar <alexandra@sommar.se>
2016-12-22 15:24:44 +0100

    småfix

--- header.php
+++ header.php
@@ -2,10 +2,10 @@
 				<section class="header__text">
 					<div class="header__text--title">
 						<span>Volvo <i class="fa fa-heart" aria-hidden="true"></i> Eco</span>
-					</div> <!-- .header__text--title -->
+					</div> <!-- .header__text title -->
 					<div class="header__text--text">
 						<span>Omtanke om vår livsmiljö.</span>
-					</div> <!-- .header__text--text -->
+					</div> <!-- .header__text text -->
 				</section> <!-- .header__text -->
 		</header>
-	</div>
+	</div> <!-- .bg-img -->
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<div class="bg-img">
 		<?php include "include/header-navigation-menu.php"; ?>
 		<?php include "header.php"; ?>
-	</div>
+	
 		
 	<!-- Message -->
 
