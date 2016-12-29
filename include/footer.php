7ce02ec437fbba637fa3c581ebff2eaba8750abd
Alexandra Sommar <alexandra@sommar.se>
2016-12-29 15:39:47 +0100

    Lägger till lite aria-labels

--- include/footer.php
+++ include/footer.php
@@ -11,10 +11,10 @@
 		</nav> <!-- .footer__nav -->
 		<nav class="footer__nav">
 			<ul class="footer__nav--responsive-icon">
-				<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-				<li><a href="#"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
-				<li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-				<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+				<li><a href="#" aria-label="Facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+				<li><a href="#" aria-label="Youtube"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
+				<li><a href="#" aria-label="Instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+				<li><a href="#" aria-label="Twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
 			</ul>
 		</nav> <!-- .footer__nav -->
 		<div class="footer__logo">
