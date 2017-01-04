5bbaac586c54f2976d7d3c3382082a05df9dbfca
Alexandra Sommar <alexandra@sommar.se>
2017-01-04 19:02:21 +0100

    Lite slutdiv-kommentarer

--- car.php
+++ car.php
@@ -11,24 +11,24 @@
 			<h3>Lorem ipsum dolo</h3>
 			<p>At erat per, urna auctor nullam arcu leo at ultricies, ante id quisque vitae pretium mi, mus rutrum rutrum unde lorem arcu. Et lectus egestas eros diam consectetuer pharetra. Rutrum neque, non nunc habitant risus cras, vel suscipit fringilla dui illo velit consectetuer.</p>
 		</section>
-	</div>
+	</div> <!-- .message__container -->
 	<div class="car__flex">
 		<div class="car__picture">
 			<img src="img/volvo_car.jpg" alt="">
-		</div>
+		</div> <!-- .car__picture -->
 		<div class="car__text">
 			<p>
 				At erat per, urna auctor nullam arcu leo at ultricies, ante id quisque vitae pretium mi, mus rutrum rutrum unde lorem arcu. Et lectus egestas eros diam consectetuer pharetra. Rutrum neque, non nunc habitant risus cras, vel suscipit fringilla dui illo velit consectetuer. Quia mattis ac suspendisse, eu mattis nulla ullamcorper, magna enim integer convallis sed, in wisi blandit quam eget vitae. Nulla primis mattis curabitur, dolor condimentum. 
 			</p>
-		</div>
-	</div>
+		</div> <!-- .car__text -->
+	</div> <!-- .car__flex -->
 	
 	<div class="message__container car__message">
 		<section class="message">
 			<h3>Lorem ipsum dolo</h3>
 			<p>At erat per, urna auctor nullam arcu leo at ultricies, ante id quisque vitae pretium mi, mus rutrum rutrum unde lorem arcu.</p>
 		</section>
-	</div>
+	</div> <!-- .message__container .car__message -->
 
 	
 	<!-- Footer -->
